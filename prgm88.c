768cec57581b0e0f484c60f21ba430bb5914b354
Ragupathi153 <32413458+Ragupathi153@users.noreply.github.com>
2018-03-12 10:17:55 +0000

    Create prgm88.c

---
+++ prgm88.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,gcd=0,i,lcm=0;
+    printf("enter the two numbers:\n");
+    scanf("%d%d",&n1,&n2);
+    for(i=1;i<=n1&&i<=n2;i++)
+    {
+        if(n1%i==0 &&n2%i==0)
+        {
+            gcd=i;
+        }
+        
+    }
+    lcm=(n1*n2)/gcd;
+    printf("%d",lcm);
+    return 0;
+}
