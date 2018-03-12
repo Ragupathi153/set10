1d92c4f9ca476c2d97f54996c648f0ec3de6691e
Ragupathi153 <32413458+Ragupathi153@users.noreply.github.com>
2018-03-12 11:01:38 +0000

    Create prgm93.c

---
+++ prgm93.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   int n,a;
+   printf("enter the temperature in celsuis:\n");
+   scanf("%d",&n);
+   a=n+273;
+   printf("temperature in kelvin: %d",a);
+   
+    return 0;
+}
